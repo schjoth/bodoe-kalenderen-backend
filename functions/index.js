1f4d8b595f3e2c5c80b18df62be934366936db53
André Schjøth <aschjoth@gmail.com>
2022-01-24 18:35:27 +0100

    fix: cors issue

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,38 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-exports.scrape = functions.https.onRequest(async (request, respone) => {
-	const axiosConfig = {
-		url: "https://tikkio.com/in/bodo",
-	};
+const corsModule = require("cors");
+const cors = corsModule({ origin: "https://bodokal.web.app" });
 
-	const getEventData = async () =>
-		await axios(axiosConfig).then((res) => {
-			const $ = cheerio.load(res.data);
-			const data = [];
-			$(".top-event").map((index, element) => {
-				const link = $(element).attr("href");
-				const title = $(element).find(".title").attr("title");
-				const description = $(element)
-					.find(".location")
-					.html()
-					.split("<br>");
-				const date = description[0];
-				const location = description[1];
-				const image = $(element).find(".image").attr("data-src");
-				data.push({ link, title, date, location, image });
+const axiosConfig = {
+	url: "https://tikkio.com/in/bodo",
+};
+
+exports.scrape = functions.https.onRequest((request, response) => {
+	cors(request, response, async () => {
+		const getEventData = async () =>
+			await axios(axiosConfig).then((res) => {
+				const $ = cheerio.load(res.data);
+				const data = [];
+				$(".top-event").map((index, element) => {
+					const link = $(element).attr("href");
+					const title = $(element).find(".title").attr("title");
+					const description = $(element)
+						.find(".location")
+						.html()
+						.split("<br>");
+					const date = description[0];
+					const location = description[1];
+					const image = $(element).find(".image").attr("data-src");
+					data.push({ link, title, date, location, image });
+				});
+
+				functions.logger.log(data);
+				return JSON.stringify(data);
 			});
 
-			functions.logger.log(data);
-			return JSON.stringify(data);
-		});
-
-	respone.send(await getEventData());
+		response.send(await getEventData());
+	});
 });
